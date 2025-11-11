93939a1d0acd7ca794eb6d37e09594ca130485a9
ratataque <ewanrimmely@outlook.fr>
2025-11-11 02:20:19 +0100

    update dot files

---
+++ sesh/sesh.sh
@@ -0,0 +1,12 @@
+tmux attach -t "dev" \; run-shell "sesh connect \"$(
+ sesh list | fzf-tmux -p 55%,60% \
+ --no-sort --border-label ' sesh ' --prompt '⚡  ' \
+ --header '  ^e all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find ^a mkdir' \
+ --bind 'ctrl-e:change-prompt(⚡  )+reload(sesh list)' \
+ --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t)' \
+ --bind 'ctrl-g:change-prompt(⚙️   )+reload(sesh list -c)' \
+ --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z)' \
+ --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -t d -E .cache . / | sed "s,^$HOME,~,")' \
+ --bind 'ctrl-d:execute(tmux kill-session -t {})+change-prompt(⚡  )+reload(sesh list)' \
+ --bind 'ctrl-a:execute(mkdir -p {q})+change-prompt(⚡  )+reload(sesh list)' \
+ )\""
